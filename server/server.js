85d5ebeedaa2f6181f34c2bc3ac17099d0053b06
AlexCapilla <acapilla93@gmail.com>
2024-05-23 13:03:47 -0700

    updated server route

--- server/server.js
+++ server/server.js
@@ -7,25 +7,7 @@ app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./server/routes/htmlRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
 
-
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// console.log("Current working directory:", process.cwd());
-// console.log("Serving static files from:", path.join(__dirname, '../client/dist'));
-
-// // Serve static files from the 'dist' directory
-// app.use(express.static(path.join(__dirname, '../client/dist')));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// require('./routes/htmlRoutes')(app);
-
-// app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
